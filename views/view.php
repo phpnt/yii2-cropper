ce8590ed0eb10e2aca00610f0a703a64280a1aa7
phpNT <admin@phpnt.com>
2016-10-31 08:49:55 +0500

    Update view.php

--- views/view.php
+++ views/view.php
@@ -1,121 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phpNT - http://phpnt.com
- * Date: 04.08.2016
- * Time: 19:08
- */
-/* @var $modelName int */
-/* @var $id int */
-/* @var $object_id int */
-/* @var $image_id int */
-/* @var $images_num int */
-/* @var $images_label string */
-/* @var $images_temp string */
-/* @var $imageSmallWidth string */
-/* @var $imageSmallHeight string */
-/* @var $idObject int */
-/* @var $createImageText string */
-/* @var $updateImageText string */
-/* @var $deleteImageText string */
-/* @var $frontendUrl string */
-/* @var $baseUrl string */
-/* @var $imagePath string */
-/* @var $noImage string */
-/* @var $loaderImage string */
-/* @var $backend boolean */
-/* @var $imageClass string */
-/* @var $buttonDeleteClass string */
-/* @var $imageContainerClass string */
-/* @var $formImagesContainerClass string */
+use yii\bootstrap\Modal;
+use yii\bootstrap\Html;
+use yii\web\View;
 
-/* @var $image \phpnt\cropper\models\Photo */
-/* @var $imagesObject array */
+/* @var $this View */
+/* @var $widget \phpnt\cropper\ImageLoadWidget */
 /* @var $modelImageForm \phpnt\cropper\models\ImageForm */
+/* @var $attribute string */
+/* @var $imagePath string */
 
-use yii\bootstrap\ActiveForm;
-use yii\widgets\Pjax;
-use yii\helpers\Html;
-use yii\helpers\Url;
-use phpnt\bootstrapNotify\BootstrapNotify;
+$render = ($widget->images_num == 1) ? '_image' : '_image-many';
 
-Pjax::begin([
-    'id' => 'images-widget-'.$id,
-    'enablePushState' => false,
+echo $this->render(
+    $render,
+    [
+        'modelName'                 => $widget->modelName,
+        'id'                        => $widget->id,
+        'object_id'                 => $widget->object_id,
+        'images_num'                => $widget->images_num,
+        'images_label'              => $widget->images_label,
+        'images_temp'               => $widget->images_temp,
+        'imageSmallWidth'           => $widget->imageSmallWidth,
+        'buttonClass'               => $widget->buttonClass,
+        'previewSize'               => $widget->previewSize,
+        'imageSmallHeight'          => $widget->imageSmallHeight,
+        'imagesObject'              => $widget->imagesObject,
+        'modelImageForm'            => $modelImageForm,
+        'createImageText'           => $widget->createImageText,
+        'updateImageText'           => $widget->updateImageText,
+        'deleteImageText'           => $widget->deleteImageText,
+        'frontendUrl'               => $widget->frontendUrl,
+        'baseUrl'                   => $widget->baseUrl,
+        'imagePath'                 => $widget->imagePath,
+        'noImage'                   => $widget->noImage,
+        'loaderImage'               => $widget->loaderImage,
+        'backend'                   => $widget->backend,
+        'imageClass'                => $widget->classesWidget['imageClass'],
+        'buttonDeleteClass'         => $widget->classesWidget['buttonDeleteClass'],
+        'imageContainerClass'       => $widget->classesWidget['imageContainerClass'],
+        'formImagesContainerClass'  => $widget->classesWidget['formImagesContainerClass'],
+    ]);
+
+Modal::begin([
+    'size' => $widget->sizeModal,
+    'header' => '<h2>'.$widget->headerModal.'</h2>',
+    'footer' => Html::button('Применить', ['class' => 'btn btn-primary crop-submit']).Html::button('Отмена', ['class' => 'btn btn-default', 'onclick' => '$("#modal-'.$widget->id.'").modal("hide")']),
+    'toggleButton' => false,
+    'options' => [
+        'id' => 'modal-'.$widget->id,
+    ]
 ]);
 ?>
-<?= BootstrapNotify::widget(); ?>
-<?php
-if(isset($isImage) && $isImage != '0'):
-    $image =  Html::img('/'.$isImage);
-else:
-    $image = Html::img($noImage);
-endif;
-$form = ActiveForm::begin(
-    [
-        'action' => Url::to(['/images/autoload-image']),
-        'options' => [
-            'enctype' => 'multipart/form-data',
-            'data-pjax' => true,
-            'id' => 'image-form-'.$id,
-            'timeout' => '7000'
-        ]
-    ]);
-?>
-<?= $form->field($modelImageForm, 'image', ['template' => '<div id="crop-url-'.$id.'" class="btn-file">{input}</div>'])
-    ->input('file', ['id' => 'imageform-image-'.$id, 'onchange' => 'loadFileMany(event)'])->label(false)->error(false); ?>
-<?php
-if ($imagesObject):
-    foreach($imagesObject as $image):
-        $imageFileSmall = $frontendUrl.$image->file_small;
-        ?>
-        <div class="<?= $imageContainerClass ?> image-padding">
-            <?= Html::button('', ['class' => $buttonDeleteClass, 'onClick' => "window.idImage = '".$image->id."'; deleteImageMany(event);"]); ?>
-            <?= Html::img($imageFileSmall, ['class' => $imageClass]); ?>
-            <?= Html::button($updateImageText, ['class' => 'btm btn-info', 'style' => 'width: 100%;', 'onclick' => "window.idImage = '".$image->id."'; $('#imageform-image-$id').click();"]) ?>
-        </div>
-        <?php
-    endforeach;
-endif;
-?>
-<?php
-$numObjects = count($imagesObject);
-if($numObjects < $images_num):
-    ?>
-    <div class="<?= $imageContainerClass; ?> image-padding">
-        <?= Html::img($noImage, ['class' => $imageClass, 'onclick' => "window.idImage = 0; $('#imageform-image-$id').click();"]); ?>
-        <?= Html::button($createImageText, ['class' => 'btm btn-info', 'style' => 'width: 100%;', 'onclick' => " window.idImage = 0; $('#imageform-image-$id').click();"]) ?>
+    <div class="crop-image-container-<?= $widget->id ?>">
+
+        <?= Html::img('', [
+            'id' => 'previewImg-'.$widget->id,
+            'class' => 'cropper-image img-responsive',
+            'alt' => 'crop-image',
+            'style' => 'width: 100%'
+        ]) ?>
     </div>
-    <?php
-endif;
-?>
 <?php
-echo Html::input('hidden', 'imageData[modelName]', $modelName);
-echo Html::input('hidden', 'imageData[id]', $id);
-echo Html::input('hidden', 'imageData[object_id]', $object_id);
-echo Html::input('hidden', 'imageData[image_id]', null, ['id' => 'image_id-'.$id]);
-echo Html::input('hidden', 'imageData[images_num]', $images_num);
-echo Html::input('hidden', 'imageData[images_label]', $images_label);
-echo Html::input('hidden', 'imageData[images_temp]', $images_temp);
-echo Html::input('hidden', 'imageData[imageSmallWidth]', $imageSmallWidth);
-echo Html::input('hidden', 'imageData[imageSmallHeight]', $imageSmallHeight);
-echo Html::input('hidden', 'imageData[createImageText]', $createImageText);
-echo Html::input('hidden', 'imageData[updateImageText]', $updateImageText);
-echo Html::input('hidden', 'imageData[deleteImageText]', $deleteImageText);
-echo Html::input('hidden', 'imageData[buttonClass]', $buttonClass);
-echo Html::input('hidden', 'imageData[previewSize]', $previewSize);
-echo Html::input('hidden', 'imageData[frontendUrl]', $frontendUrl);
-echo Html::input('hidden', 'imageData[baseUrl]', $baseUrl);
-echo Html::input('hidden', 'imageData[imagePath]', $imagePath);
-echo Html::input('hidden', 'imageData[noImage]', $noImage);
-echo Html::input('hidden', 'imageData[loaderImage]', $loaderImage);
-echo Html::input('hidden', 'imageData[backend]', $backend);
-echo Html::input('hidden', 'imageData[imageCrop]', null, ['id' => 'imageCrop-'.$id]);
-echo Html::input('hidden', 'imageData[imageClass]', $imageClass);
-echo Html::input('hidden', 'imageData[buttonDeleteClass]', $buttonDeleteClass);
-echo Html::input('hidden', 'imageData[imageContainerClass]', $imageContainerClass);
-echo Html::input('hidden', 'imageData[formImagesContainerClass]', $formImagesContainerClass);
-ActiveForm::end();
-?>
-<?php
-Pjax::end();
+Modal::end();
+
